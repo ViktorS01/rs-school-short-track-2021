a4f633aade05b984e2beb9b2684db828fb158eec
ViktorS01 <75483754+ViktorS01@users.noreply.github.com>
2021-04-13 00:49:59 +0400

    feat: add task delete digit

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,21 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const arr = n.toString().split('');
+  let numArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    numArr[i] = '';
+    for (let j = 0; j < arr.length; j++) {
+      if (j !== i) {
+        numArr[i] += arr[j];
+      }
+    }
+    numArr[i] = Number(numArr[i]);
+  }
+  numArr = numArr.sort((a, b) => a - b);
+  return numArr[numArr.length - 1];
 }
 
 module.exports = deleteDigit;
